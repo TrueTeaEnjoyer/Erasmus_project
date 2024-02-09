d332664947290189fde7cf4a5829ae0f703f14cd
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2024-02-09 10:11:16 +0100

    Add files via upload

---
+++ daswerk/erasmus/spiders/myspider.py
@@ -0,0 +1,36 @@
+import scrapy
+
+
+class MySpider(scrapy.Spider):
+    name = 'daswerkSpider'
+    start_urls = ['https://www.daswerk.org/programm/']
+
+    custom_settings = {
+        'FEEDS': {
+            'oDaswerk.json': {
+                'format': 'json',
+                'overwrite': True,  # If the file already exists, it will overwrite it
+            },
+        },
+    }
+
+    def parse(self, response):
+        # Scraping the main page
+        self.logger.info(f"Scraping main page: {response}")
+
+        # Extracting links to individual party pages
+        links = response.css('a.preview-item--link::attr(href)').getall()
+
+        # Following each link to the party page
+        for link in links:
+            yield response.follow(link, callback=self.parse_event)
+
+    def parse_event(self, response):
+        # Extracting the title from the party page
+        title = response.css('p.main--header-title::text').get()
+
+        # Yielding the result with 'Link' and 'Title'
+        yield {
+            'Link': response.url,
+            'Title': title,
+        }
